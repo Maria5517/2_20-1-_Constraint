70f7de9d7c3990aa8a98942205043548328ff621
Maria Masljonok <maria@onoffapp.com>
2021-05-17 20:34:45 +0300

    Initial Commit

---
+++ 2_20_SwiftBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2_20_SwiftBook
+//
+//  Created by Maria Masljonok on 17.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
